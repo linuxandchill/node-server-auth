94904576639116becc842d65ec2d3087059b443e
ck <thecurrencykid@gmail.com>
2016-10-11 12:52:14 -0400

    bcrypt compare method

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,25 @@ const passport = require('passport');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy; 
 const ExtractJwt = require('passport-jwt').ExtractJwt; 
+const LocalStrategy = require('passport-local'); 
+
+//setup options for local strategy
+const localOptions={ usernameField: 'email'  }
+
+//create Local Strategy
+const localLogin = new LocalStrategy(localOptions, function(email, password,done){
+  //verify email and pw, call done w this user
+  //if correct
+  //otherwise call done w false
+  User.findOne({ email: email }, function(err, user) {
+    if(err) {return done(err); }
+
+    if(!user){return done(null, false);}
+
+    // compare passwords - is 'password' equal to user.password ?
+    // gotta compare plain text pw to encrcypted pw
+  }
+}); 
 
 //setup options for JWT Strategy
 const jwtOptions= {
@@ -12,16 +31,16 @@ const jwtOptions= {
 
 //create JWT Strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
- User.findById(payload.sub, function(err,user){
-  if(err) {return done(err,false); }
+  User.findById(payload.sub, function(err,user){
+    if(err) {return done(err,false); }
 
-  if(user){
-    done(null, user);
-  } else {
-    done(null, false); 
-  }
-  
- }); 
+    if(user){
+      done(null, user);
+    } else {
+      done(null, false); 
+    }
+
+  }); 
 });
 
 //tell passport to use this strategy
